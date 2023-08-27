75696093d4a8f46072f71cb0c999b08af579bffb
AL <ayelay1995.mm@gamil.com>
2023-08-27 23:17:31 +0630

    forcast

--- src/index.js
+++ src/index.js
@@ -21,22 +21,40 @@ function getformatDate(timestamp) {
   return `${day} ${hour}:${minute}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForcast(response) {
-  console.log(response.data.daily);
+  let forcast = response.data.daily;
   let forcastElement = document.querySelector("#forcast");
   let forcastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forcastHTML =
-      forcastHTML +
-      `<div class="col">
-                <div class="forecast-day">${day}</div>
-                <div class="forecast-temperature"><span class="forecast-temperature-max">34°</span><span class="forecast-temperature-min"> 19°</span></div>
+
+  forcast.forEach(function (forcastDay, index) {
+    if (index < 7) {
+      forcastHTML =
+        forcastHTML +
+        `<div class="col">
+                <div class="forecast-day">${formatDay(forcastDay.time)}</div>
+                <div class="forecast-temperature"><span class="forecast-temperature-max">${Math.round(
+                  forcastDay.temperature.maximum
+                )}°</span><span class="forecast-temperature-min"> ${Math.round(
+          forcastDay.temperature.minimum
+        )}°</span></div>
+  
                 <div class="forecast-icon">
-                  <i class="fa-sharp fa-solid fa-cloud"></i>
+                 <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                   forcastDay.condition.icon
+                 }.png" alt="shower-rain-day.png">
                 </div>
               </div>
+
+              
         `;
+    }
   });
 
   forcastHTML = forcastHTML + `</div>`;
@@ -44,15 +62,12 @@ function displayForcast(response) {
 }
 
 function getForcast(coordinates) {
-  console.log(coordinates);
   let apiKey = "ob09c1d70a7b42abct8580f52909b5a3";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayForcast);
 }
 
 function displayWeatherCondition(response) {
-  console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.city;
   document.querySelector("#weather-description").innerHTML =
     response.data.condition.description;
